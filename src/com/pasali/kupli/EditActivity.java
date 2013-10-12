d3091676eb05f280385bffb8760256e614b89ea8
Mehmet Başal <mhmtbsl@bil.omu.edu.tr>
2013-10-12 22:51:03 +0300

    editable:ok

--- src/com/pasali/kupli/EditActivity.java
+++ src/com/pasali/kupli/EditActivity.java
@@ -15,7 +15,6 @@ public class EditActivity extends Activity {
 	private Account acc = null;
 	private AccDAO editDao;
 	
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
